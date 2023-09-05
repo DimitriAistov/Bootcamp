e68b0528ae1e7d6f9abaae694aca2bbee4fdf87e
Dimitri <99058899@mydavinci.nl>
2023-09-05 10:28:31 +0200

    Les6 opdracht 1,2

---
+++ Deel_1/Les_6/uitwerkingen/Les6_opdracht1.py
@@ -0,0 +1,5 @@
+if x = 18:
+        print('de waarde van x = 18')
+
+#Wat er niet klopt is dat de X waarde nog niet is aangegeven maar wel wordt gebruikt in een if
+#er moet ook een tweede '=' bij de if x =18 ->  if x == 18
---
+++ Deel_1/Les_6/uitwerkingen/Les6_opdracht2.py
@@ -0,0 +1,10 @@
+varA = input('Wat is het eerste getal? ')
+varB = input('Wat is het tweede getal? ')
+
+
+if varA > varB:
+    print('Variabele a is het grootst want ', varA, 'is groter dan', varB)
+if varB > varA:
+    print('Variabele b is het grootst want ', varB, 'is groter dan', varA)
+if varA == varB:
+    print('Variabele a en b zijn gelijk.')
