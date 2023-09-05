e850b2d1553f60e7dfb4b1d263abdc53d8649de4
Dimitri <99058899@mydavinci.nl>
2023-09-05 10:17:39 +0200

    Les5 opdracht3,4

---
+++ Deel_1/Les_5/uitwerkingen/Les5_opdracht 4.py
@@ -0,0 +1,6 @@
+import math
+
+straal = int(input('Wat is de straal van de cirkel? '))
+
+
+print(straal * straal * math.pi)
---
+++ Deel_1/Les_5/uitwerkingen/Les5_opdracht3.py
@@ -0,0 +1,6 @@
+nummer1 = int(input('voer eerste getal in ' ))
+nummer2 = int(input('voer tweede getal in ' ))
+nummer3 = int(input('voer derde getal in ' ))
+
+gemiddelde = (nummer1 + nummer2 + nummer3)/3
+print (gemiddelde)
